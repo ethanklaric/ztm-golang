643f2c0374e089fa2c731f015c67b968ca711bb5
Ethan Klaric <s3920200@student.rmit.edu.au>
2024-11-04 00:37:29 +1100

    variables lesson complete

--- src/lectures/exercise/variables/variables.go
+++ src/lectures/exercise/variables/variables.go
@@ -25,5 +25,19 @@ package main
 import "fmt"
 
 func main() {
-}
+	var favColour string = "green"
+	fmt.Println("my favourite colour is ", favColour)
 
+	birthYear, ageYears := 2002, 22
+	fmt.Println("i was born in ", birthYear, ", i am ", ageYears, "years old")
+
+	var (
+		firstInitial = "E"
+		lastInitial  = "K"
+	)
+	fmt.Println("my initials are ", firstInitial, lastInitial)
+
+	var ageDays int
+	ageDays = ageYears * 365
+	fmt.Println("i've been alive for ", ageDays, "days.")
+}
